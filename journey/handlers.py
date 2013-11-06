4cb93ee5ddf28625f76fdaf82dd4eb950f2bd640
Rocker_Raj <rishirajsingh.jhelumi@students.iiit.ac.in>
2013-11-06 12:30:52 +0530

    Removed unwanted handler

--- journey/handlers.py
+++ journey/handlers.py
@@ -1,12 +1,1 @@
-import webapp2
 
-class HomePage(webapp2.RequestHandler):
-
-    def get(self,month,year):
-        self.response.headers['Content-Type'] = 'application/json'
-        
-        obj = {
-               'success': year, 
-               'payload': month,
-            } 
-        self.response.write(obj)
