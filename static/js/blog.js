0f297cbc42ff0404fbacfdeb0fe2dcc1988bb6a1
Rocker_Raj <rishirajsingh.jhelumi@students.iiit.ac.in>
2013-11-22 00:12:52 +0530

    Added Basic Blog

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,28 +1,24 @@
 var newUser;
 
 var newBlogDetails = function(journeyId){
-	this.accessToken;
+	this.accessToken = null;
 	this.journeyId = journeyId;
-	this.journeyDetails;
-	this.journeyMilestones;
+	this.journeyDetails = null;
+	this.journeyMilestones = null;
 	this.error = null;
 	this.blogDetails = [];
+	
 	this.verifyAccessToken = function(){
 		self = this;
-		var url = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="+this.accessToken;
+		var url = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + this.accessToken;
 		$.ajax({
 			url: url,
 			type: 'GET',
 			async:false,
 			error: function(data) {
-					console.log("DTATAAA");
-					console.log(data['responseText']);
-				    var obj = $.parseJSON(data['responseText']);
-					self.error = obj['error'];
-					console.log(self.error);
+				self.error = data['responseJSON']['error'];
 			},
 		}).done(function(data) {
-			console.log(data);
 		},"json");
 	};
 	
@@ -34,7 +30,7 @@ var newBlogDetails = function(journeyId){
 			type: 'GET',
 			async:false,
 		}).done(function(data) {
-			console.log(data);
+			console.log(data['access_token']);
 			self.accessToken = data['access_token'];
 		},"json");
 	};
@@ -42,7 +38,6 @@ var newBlogDetails = function(journeyId){
 	this.loadJourney = function(){
 		self = this;
 		var url = "/journey/get/" + this.journeyId;
-
 		$.ajax({
 			url: url,
 			async:false,
@@ -52,57 +47,72 @@ var newBlogDetails = function(journeyId){
 		},"json");
 
 	};
+	
+	this.getImageSource = function(imageId){
+		
+		var baseUrl = "http://thetravelerbook.appspot.com";
+		var imgSource = baseUrl + "/image/" + imageId;
+		return imgSource;
+		
+	}
+	
+	this.getGeneratedBlog = function(blogId){
+		
+		var blog = {};
+		blog['kind'] = "blogger#post";
+		blog['blog'] = {};
+		blog['blog']['id'] = blogId;
+		blog['title'] = this.journeyDetails["name"];
+		
+		var content = "";
+		
+		content += '<h2>' + this.journeyDetails['name'] + '</h2>';
+		
+		var journeyTime = new Date(this.journeyDetails['time'] * 1000);
+		content += '<h5>' + journeyTime.toDateString() + '</h5>';
+		
+		content += '<p>' + this.journeyDetails['description'] + '</p>';
+		content += '<img src = "' + this.getImageSource(this.journeyDetails['image']) + '" />';
+		
+		for(i=0;i<this.journeyDetails['path'];i++){
+			var milestoneContent = "";
+			
+			content += milestoneContent;
+		}
+		
+		blog['content'] = content;
+		return blog;
+		
+	}
 
 	this.postJourney = function(){
-		self=this;
-		console.log(self.accessToken);
+		
+		self = this;
 		var currBlog = this.blogDetails[0];
-		var journeyDate = new Date( this.journeyDetails['time'] *1000);
-		var url = "https://www.googleapis.com/blogger/v3/blogs/" +currBlog["id"]+"/posts/";
-		var longString = "{'kind':'blogger#post'  , 'blog': {'id':  " + currBlog['id'] + " }, \
-						 'title': '" + this.journeyDetails["name"] + "', 'content': '"+this.journeyDetails['description']+" <br><br> Dated : <b>"+journeyDate+"<b><br>'  ,  \
-			'image': { 'url': 'http://1.bp.blogspot.com/-dmCxXMKIIa0/Uid4grOEDSI/AAAAAAAADzk/nhzmCUTJhZs/s320/Bliss-Windows-XP.png' } } ";
+		var journeyDate = new Date( this.journeyDetails['time'] * 1000);
+		var url = "https://www.googleapis.com/blogger/v3/blogs/" + currBlog["id"] + "/posts/";
+		
+		var blogToPost = JSON.stringify(this.getGeneratedBlog(currBlog['id']));
+		console.log(blogToPost);
+		
 		$.ajax({
 			url: url,
 			async:false,
 			type: 'POST',
-			data: longString,
+			data: blogToPost,
 			beforeSend: function (request)
 			{
-				request.setRequestHeader("Authorization","Bearer "+self.accessToken);
+				request.setRequestHeader("Authorization","Bearer " + self.accessToken);
 				request.setRequestHeader("Content-Type","application/json");
 			},
 		}).done(function(data) {
 			console.log(data);
 		});
-		for(var i = 0 ; i<this.journeyDetails['path'].length; ++i ){
-			var journeyDate = new Date( this.journeyDetails['path'][i]['time'] *1000);
-			var url = "https://www.googleapis.com/blogger/v3/blogs/" +currBlog["id"]+"/posts/";
-			var longString = "{'kind':'blogger#post'  , 'blog': {'id':  " + currBlog['id'] + " }, \
-							 'title': '<b>" + this.journeyDetails['path'][i]['location'] + "<b>', 'content': '"+this.journeyDetails['path'][i]['description']+" <br><br> Dated : <b>"+journeyDate+"<b><br>'  ,  \
-				'image': { 'url': 'http://1.bp.blogspot.com/-dmCxXMKIIa0/Uid4grOEDSI/AAAAAAAADzk/nhzmCUTJhZs/s320/Bliss-Windows-XP.png' } } ";
-			$.ajax({
-				url: url,
-				async:false,
-				type: 'POST',
-				data: longString,
-				beforeSend: function (request)
-				{
-					request.setRequestHeader("Authorization","Bearer "+self.accessToken);
-					request.setRequestHeader("Content-Type","application/json");
-				},
-			}).done(function(data) {
-				console.log(data);
-			});
-		}
-	};
-	
-	
-	
-	
-	
+		
+	}
 	
 	this.getUserDetail = function(){
+		
 		var url = "https://www.googleapis.com/blogger/v3/users/self";
 		self = this;
 		$.ajax({
@@ -111,15 +121,14 @@ var newBlogDetails = function(journeyId){
 			async:false,
 			beforeSend: function (request)
 			{
-				//request.setRequestHeader("Authorization","Bearer ya29.1.AADtN_VXVsrqd4u-ccW2wMOZqB5ZKDJVO6g1p9jn__z6XG9szep8puYMqIr4mcFdK9X-fA");
-				request.setRequestHeader("Authorization","Bearer "+self.accessToken);
+				request.setRequestHeader("Authorization","Bearer " + self.accessToken);
 			},
 		}).done(function(data) {
-			console.log(data)
 		},"json");
 	}
 	
 	this.getUserBlogDetails = function(){
+		
 		var url = "https://www.googleapis.com/blogger/v3/users/self/blogs";
 		self = this;
 		$.ajax({
@@ -128,34 +137,26 @@ var newBlogDetails = function(journeyId){
 			async:false,
 			beforeSend: function (request)
 			{
-				//request.setRequestHeader("Authorization","Bearer ya29.1.AADtN_VXVsrqd4u-ccW2wMOZqB5ZKDJVO6g1p9jn__z6XG9szep8puYMqIr4mcFdK9X-fA");
-				request.setRequestHeader("Authorization","Bearer "+self.accessToken);
+				request.setRequestHeader("Authorization","Bearer " + self.accessToken);
 			},
 		}).done(function(data) {
-			obj = data;
-			for(var i=0;i<obj["items"].length;++i){
-				temp = {};
-				temp["name"] = obj["items"][i].name;
-				temp["id"] = obj["items"][i].id;
-				self.blogDetails.push(temp);
+			for(var i=0;i<data["items"].length;++i){
+				self.blogDetails.push(data["items"][i]);
 			}
 		},"json");
 	}
 	
 	this.init = function(){
-		console.log("yaha bhi aaya ");
 		this.getAccessToken();
 		this.verifyAccessToken();
-		console.log(this.error);
+		
 		if(this.error == null){
-			console.log("going Great");
 			this.getUserDetail();
 			this.getUserBlogDetails();
 			this.loadJourney();
 			this.postJourney();
 		}else{
 			console.log(this.error);
-			
 		}
 	};
 }
